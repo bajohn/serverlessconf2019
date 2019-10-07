672fb6cfb46d2654ac5ad82bbaf9064401b9609d
Stanley Zheng <stanley@zheng.nyc>
2019-10-07 13:29:57 -0400

    updating accumulation data

--- example/parse.js
+++ example/parse.js
@@ -1,4 +1,4 @@
-
+const slugify = require('slugify');
 
 const request_1 = require('./85-broadst.json');
 const request_2 = require('./400-broadway.json');
@@ -10,13 +10,26 @@ console.log(request_1['features'][0])
 
 
 
-function aggregate_keys () {
-
+function aggregate_results (payload) {
+    const features = payload['features']
+    return features.reduce((a, b)=> {
+        // console.log(b)
+        let key = `${b.properties.on_street}_${b.properties.from_street}_${b.properties.to_street}`
+        key = slugify(key);
+        // console.log(key)
+        if (a.hasOwnProperty(key)) {
+            a[key].append(b);
+        }
+        b[key] = [b]
+        return a
+    }, {})
 }
 
 
 function main() {
+
+    return aggregate_results(request_1)
 }
 
 
-console.log(main())
+console.log(main()) 
