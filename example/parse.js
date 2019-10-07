cc6b50e74ad1fbe89b6948fedf175b29083df7ac
Stanley Zheng <stanley@zheng.nyc>
2019-10-07 13:13:37 -0400

    updating parse.js

---
+++ example/parse.js
@@ -0,0 +1,22 @@
+
+
+const request_1 = require('./85-broadst.json');
+const request_2 = require('./400-broadway.json');
+const request_3 = require('./32-monroe-st.json');
+
+
+
+console.log(request_1['features'][0])
+
+
+
+function aggregate_keys () {
+
+}
+
+
+function main() {
+}
+
+
+console.log(main())
